de9d7a5d2f060136311822f8da1edfc5c4e21fe5
anish <22464141+aykrishnan@users.noreply.github.com>
2019-10-07 22:34:10 -0500

    Update Simulation.java

--- src/Simulation.java
+++ src/Simulation.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 public class Simulation {
+    
+    // Is this method necessary?
     public static void print(String s) {
         System.out.println(s);
     }
@@ -9,7 +11,10 @@ public class Simulation {
     private static ArrayList<U1> loadedU1 = new ArrayList<>();
     private static ArrayList<U2> loadedU2 = new ArrayList<>();
     private int totalCost = 0;
+    
+    // Why is there an empty consructor that throws an Exception?
     public Simulation() throws Exception{}
+    
         public ArrayList <Item> loadItems(String s) throws Exception {
         File file = new File(s);
         Scanner inp = new Scanner(file);
@@ -37,6 +42,11 @@ public class Simulation {
         }
         return loadedU1;
         }
+    
+    /*
+      This method and the above contain a lot of shared code. In addition, this class is handling responsibilities that
+      maybe the U classes should handle instead. Think about the principle of encapsulation in OOP.
+    */
     public ArrayList<U2> loadu2(ArrayList<Item>inpArray){
         U2 u2 = new U2();
         while(inpArray.size()>0){
@@ -52,6 +62,8 @@ public class Simulation {
         }
         return loadedU2;
     }
+    
+    // This method could probably be broken down into multiple methods
     public int runSimulation (String rocketType, int phase) throws Exception {
         //Determining the phase
         String fileName;
@@ -63,6 +75,10 @@ public class Simulation {
                 fileName = "phase_2.txt";
                 break;
             default:
+                /*
+                  Returning -1 isn't really common in Java; it's more of a C thing. I would recommend throwing an
+                  Exception or otherwise handling this error case somehow
+                */
                 print("Invalid input (possible mis-spelling)");
                 return -1;
         } //switch
@@ -70,7 +86,7 @@ public class Simulation {
         ArrayList<Item> itemList = this.loadItems(fileName);
 
         //Determining the ship type for simulation
-        switch (rocketType) {
+        switch (rocketType) {  // Recommendation: Use the String.toLowerCase() method to make life easier
             case "u1": case "U1":
                 U1 u1 = new U1();
                 ArrayList<U1> U1Array = this.loadu1(itemList);
@@ -108,6 +124,7 @@ public class Simulation {
             default:
                 print("Invalid input (possible mis-spelling)");
                 return -1; //We can use -1 in the method call in Main to handle all invalid inputs since cost can never be negative
+                // ^ Or you could throw a custom Exception
         } //switch
 
     } //runSimulation
